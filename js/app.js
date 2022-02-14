ed3b48371c445dcb9dc826f818b3cedfed3cd2ab
bashar125824 <bashar.alrefae.7@gmail.com>
2022-02-14 16:55:42 +0200

    Create constructor Branch

---
+++ js/app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+var arr = [];
+
+function Employee(ID , FullName , Department , Level ){
+    this.ID = ID;
+    this.FullName = FullName;
+    this.Department = Department;
+    this.Level = Level;
+    arr.push(this)
+}
+
+
+
+ 
+Employee.prototype.Salary = function (Level){
+   
+    var salary;
+if(this.Level == "Senior")
+{   
+     salary = (Math.floor(Math.random() * (2000 - 1500)) + 1500) * 0.075 ;}
+
+else if (this.Level == "Mid-Senior"){
+     salary = (Math.floor(Math.random() * (1500 - 1000)) + 1000) * 0.075 ;}
+
+else if (this.Level == "Junior") {
+     salary = (Math.floor(Math.random() * (1000 - 500)) + 500) * 0.075 ;}
+    return salary;
+
+}
+
+
+Employee.prototype.render = function(){
+
+        document.writeln(`<p>${this.FullName}  :  ${this.Salary(this.Level)}</p>`);
+
+}
+
+
+
+let Ghazi = new Employee(1000, "Ghazi Samer", "Administration", "Senior");
+let Lana = new Employee(1001, "Lana Ali", "Finance", "Senior");
+let Tamara = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior");
+let Safi = new Employee(1003, "Safi Walid", "Administration", "Mid-Senior");
+let Omar = new Employee(1004, "Omar Zaid", "Development", "Senior");
+let Rana = new Employee(1005, "Rana Saleh", "Development", "Junior");
+let Hadi = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior");
+
+for (let i = 0; i < arr.length; i++) {
+   arr[i].render();
+}
+
